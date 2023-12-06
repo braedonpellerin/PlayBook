d6abe4489deade4f6d8a68e2b4458736040a11d1
braedonpellerin <bpellerin53@academic.rrc.ca>
2023-12-05 19:46:21 -0600

    Login/Logout Functionality
     Added OnClickListeners for Login/Logout buttons

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -22,5 +22,35 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        // OnClick listener to navigate the login /page activity.
+        // Displays Toast message if already signed in.
+        val login = findViewById<Button>(R.id.btnLogin)
+        login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            // If the user is not logged in, brings them to login page.
+            if (FirebaseAuth.getInstance().currentUser == null) {
+                startActivity(intent)
+            } else {  // Else displays that they are already logged in.
+                val toast = Toast.makeText(applicationContext, "Already logged in.", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+
+        // Click listener to sign out.
+        val logout = findViewById<Button>(R.id.btnLogout)
+        logout.setOnClickListener {
+            // If there is no user signed in, display that they are already signed out.
+            if (FirebaseAuth.getInstance().currentUser == null) {
+                val toast = Toast.makeText(applicationContext, "Already signed out.", Toast.LENGTH_SHORT)
+                toast.show()
+            } else { // Else sign the current user out.
+                FirebaseAuth.getInstance().signOut()
+                val toast = Toast.makeText(applicationContext, "Sign out successful.", Toast.LENGTH_SHORT)
+                finish()
+                startActivity(getIntent())
+                toast.show()
+            }
+        }
     }
 }
