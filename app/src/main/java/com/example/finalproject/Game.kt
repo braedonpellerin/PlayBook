a86fc6e827098fe3a9df6d7af64618b3d7c3199e
braedonpellerin <bpellerin53@academic.rrc.ca>
2023-12-13 00:11:22 -0600

    Added Game Class
    Used to store game data for database

---
+++ app/src/main/java/com/example/finalproject/Game.kt
@@ -0,0 +1,12 @@
+package com.example.finalproject
+
+// Game Class
+class Game (
+    val gameName: String? = null,
+    val platform: String? = null,
+    val hours: Int? = null,
+    val rating: Int? = null,
+    val completed: Boolean? = null,
+    val comments: String? = null)
+{
+}
